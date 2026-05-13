291b64abe98917cd5c784c8bf850409098993463
kothavitesh09 <kothavitesh123@gmail.com>
2026-05-13 12:02:51 +0530

    Url Shortner Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import { HeaderMegaMenu } from './Components/Navbar/HeaderMegaMenu';
 import { ProfilePage } from './Pages/Profile/ProfilePage';
+import  URLShortener  from './Pages/ShortURL/Urlshortner';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<LoginPage/>} />
             <Route path='/profile' element={<ProfilePage/>} />
+            <Route path='/shorten' element={<URLShortener/>} />
             <Route element={<PrivateRoute/>}>
 
             </Route>
